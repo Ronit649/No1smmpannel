f82ad491dbf1d5b03080d0f0d87bc57f7efa1ac8
Ronit649 <ronitk383@gmail.com>
2025-05-29 19:28:49 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to No1 SMMPannel</h1>
+      <p>This is a simple React app deployed on Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
